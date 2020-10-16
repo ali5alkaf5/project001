37e9244ca9889bac042551dd16095f91f0ef77fb
ali5alkaf5 <ali5alkaf5@gmail.com>
2020-10-16 07:25:25 +0700

    's'

---
+++ Desktop/discord bot/root/bot.js
@@ -0,0 +1,71 @@
+const Discord = require('discord.io');
+const DiscordEmbeds = require('discord.js');
+const logger = require('winston');
+const auth = require('../auth.json');
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console, {
+    colorize: true
+});
+logger.level = 'debug';
+// Initialize Discord Bot
+let bot = new Discord.Client({
+    token: auth.token,
+    autorun: true
+});
+let embed = new DiscordEmbeds.MessageEmbed();
+
+embed.setColor('#0099ff')
+    .setTitle('Some title')
+    .setURL('https://discord.js.org/')
+    .setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
+    .setDescription('Some description here')
+    .setThumbnail('https://i.imgur.com/wSTFkRM.png')
+    .addFields(
+        { name: 'Regular field title', value: 'Some value here' },
+        { name: '\u200B', value: '\u200B' },
+        { name: 'Inline field title', value: 'Some value here', inline: true },
+        { name: 'Inline field title', value: 'Some value here', inline: true },
+    )
+    .addField('Inline field title', 'Some value here', true)
+    .setImage('https://i.imgur.com/wSTFkRM.png')
+    .setTimestamp()
+    .setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
+
+
+
+bot.on('ready', function (evt) {
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
+});
+bot.on('message', function (user, userID, channelID, message, evt) {
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `!`
+    if (message.substring(0, 1) == '!') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+
+        args = args.splice(1);
+        switch(cmd) {
+            // !ping
+            case 'ping':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Pong!'
+                });
+                break;
+            // Just add any case commands if you want to..
+        }
+        bot.sendMessage({
+            to: channelID,
+            message: 'received'
+        });
+        bot.sendMessage({
+            to: channelID,
+            message: embed
+        });
+
+
+    }
+});
